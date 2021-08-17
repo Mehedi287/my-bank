c65a7cd513bf7a65155456da3dbbc7bf842e38bc
Mehedi390 <mh0816150@gmail.com>
2021-08-17 03:08:56 -0700

    loging from

--- js/primary.js
+++ js/primary.js
@@ -7,7 +7,7 @@ function getUserInfo(userInput) {
 document.getElementById('submit-btn').addEventListener('click', function () {
     const userEmail = getUserInfo('user-email');
     const userPassword = getUserInfo('user-password');
-    if (userEmail == 'mehediHasan@gmail.com' && userPassword == 23159287) {
-        window.location.href = "bank.html"
+    if (userEmail == 'mehediHasan@gmail.com' && userPassword == 1234) {
+        window.location.href = 'bank.html';
     }
 })
